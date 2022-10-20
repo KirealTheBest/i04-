5009ab6fae99e646fb61a5cdf9193bb025c50730
Kirill Grigorev <kirillgrigorev4043@gmail.com>
2022-10-20 06:13:41 +0000

    63.4

---
+++ cycles/63.4.php
@@ -0,0 +1,15 @@
+<?php
+	$arr = ['a' => 1, 'b' => 2, 'c' 
+		=> 3, 'd' => 4, 'e' => 5]; 
+
+    $x=[];
+    $y=[];
+
+    foreach ($arr as $key => $elem) {
+        $x[]=$key;
+        $y[]=$elem;
+
+    }
+    var_dump($x)'<br>';
+    var_dump($x);
+?>
