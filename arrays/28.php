0e3d96cd97b3395618559125af8c7c058ec94b07
Kirill Grigorev <kirillgrigorev4043@gmail.com>
2022-11-02 03:42:30 +0000

    28.1, 28.2.php

---
+++ arrays/28.php
@@ -0,0 +1,14 @@
+<?php //28.1
+	$arr = [1, 2, 3, 4, 5]; 
+    $key1 = 1;
+    $key2 = 2;
+    echo $x = $arr[$key1] + $arr[$key2];
+?>
+
+<?php //28.2
+    echo '<br>';
+	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+    $key = 'b';
+    echo $key;
+    
+?>
