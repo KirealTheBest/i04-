09572d3f23cef4cb4d303e94fdf41218cda56f17
Kirill Grigorev <kirillgrigorev4043@gmail.com>
2022-11-08 12:38:58 +0000

    29.1-29.7

---
+++ conditions/29.php
@@ -0,0 +1,68 @@
+<?php
+//29.1
+        $test = 0;
+        
+        if ($test > 10) {
+            echo '+';
+        } else {
+            echo '-' ;
+        }
+                        echo '<br>';
+//29.2
+        $test = 0;
+        
+        if ($test < 10) {
+            echo '+';
+        } else {
+            echo '-';
+        }
+
+                        echo '<br>';
+//29.3
+        $test = 0;
+        
+        if ($test >= 10) {
+            echo '+';
+        } else {
+            echo '-';
+        }
+                         echo '<br>';
+//29.4
+        $test = 0;
+                
+        if ($test <= 10) {
+            echo '+';
+        } else {
+            echo '-';
+        }
+
+                                 echo '<br>';
+//29.5
+        $test = 0;
+                
+        if ($test == 10) {
+            echo '+';
+        } else {
+            echo '-';
+        }
+
+                                 echo '<br>';
+//29.6
+        $test = 0;
+                
+        if ($test != 10) {
+            echo '+';
+        } else {
+            echo '-';
+        }
+                                 echo '<br>';
+//29.7
+        $test1 = 5;
+        $test2 = 7;
+                
+        if ($test1 > $test2) {
+            echo 'test1 больше!';
+        } else {
+            echo 'test2 больше!';
+        }
+?>
