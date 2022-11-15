d0336c809cbb06e3f8b6536be223fee2700540e7
Kirill Grigorev <kirillgrigorev4043@gmail.com>
2022-11-15 13:56:53 +0000

    36.1-36.3

---
+++ conditions/36.php
@@ -0,0 +1,14 @@
+<?php
+//36.1
+	$test = 10;
+	
+	if ($test == 10) {
+		echo 'Верно';
+	}
+echo '<br>';
+//36.2
+	if ($test == 0) echo "+"; else echo '-';
+echo '<br>';
+//36.3
+	if ($test == 0) echo 'верно';
+?>
