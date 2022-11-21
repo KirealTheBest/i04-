aeb6cbc88fc49f9791e45844d60ed417164ff4c6
Kirill Grigorev <kirillgrigorev4043@gmail.com>
2022-11-21 06:41:19 +0000

    40.php

---
+++ conditions/40.php
@@ -0,0 +1,34 @@
+<?php
+//40.1
+	$num = '1';
+	
+	switch ($num) {
+		case '1':
+			echo 'зима';
+		break;
+		case '2':
+			echo 'весна';
+		break;
+		case '3':
+			echo 'лето';
+		break;
+        case '4':
+			echo 'осень';
+		break;
+	}
+echo '<br>';
+?>
+<?php
+$num = 1;
+switch ($num) {
+    case 1:
+        echo 1;
+    break;
+    case 2:
+        echo 2;
+    break;
+    case 3:
+        echo 3;
+    break;
+}
+?>
